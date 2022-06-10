c17485b7efc8ace15ae00e80513e6e8669890216
Newton2541 <iomolayo2541@gmail.com>
2022-06-10 19:22:08 +0100

    changed print to prefect logger

--- 03-orchestration/homework.py
+++ 03-orchestration/homework.py
@@ -7,7 +7,7 @@ from sklearn.metrics import mean_squared_error
 from datetime import datetime
 from dateutil import relativedelta
 
-from prefect import flow, task
+from prefect import flow, task, get_run_logger
 from prefect.task_runners import SequentialTaskRunner
 from prefect.deployments import DeploymentSpec
 from prefect.orion.schemas.schedules import CronSchedule
@@ -28,46 +28,51 @@ def read_data(path):
 
 @task
 def prepare_features(df, categorical, train=True):
+    logger = get_run_logger()
+    
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     mean_duration = df.duration.mean()
     if train:
-        print(f"The mean duration of training is {mean_duration}")
+        logger.info(f"The mean duration of training is {mean_duration}")
     else:
-        print(f"The mean duration of validation is {mean_duration}")
+        logger.info(f"The mean duration of validation is {mean_duration}")
     
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
     return df
 
 @task
 def train_model(df, categorical):
+    logger = get_run_logger()
 
     train_dicts = df[categorical].to_dict(orient='records')
     dv = DictVectorizer()
     X_train = dv.fit_transform(train_dicts) 
     y_train = df.duration.values
 
-    print(f"The shape of X_train is {X_train.shape}")
-    print(f"The DictVectorizer has {len(dv.feature_names_)} features")
+    logger.info(f"The shape of X_train is {X_train.shape}")
+    logger.info(f"The DictVectorizer has {len(dv.feature_names_)} features")
 
     lr = LinearRegression()
     lr.fit(X_train, y_train)
     y_pred = lr.predict(X_train)
     mse = mean_squared_error(y_train, y_pred, squared=False)
-    print(f"The MSE of training is: {mse}")
+    logger.info(f"The MSE of training is: {mse}")
     return lr, dv
 
 @task
 def run_model(df, categorical, dv, lr):
+    logger = get_run_logger()   
+
     val_dicts = df[categorical].to_dict(orient='records')
     X_val = dv.transform(val_dicts) 
     y_pred = lr.predict(X_val)
     y_val = df.duration.values
 
     mse = mean_squared_error(y_val, y_pred, squared=False)
-    print(f"The MSE of validation is: {mse}")
+    logger.info(f"The MSE of validation is: {mse}")
     return
 
 @task
@@ -118,7 +123,8 @@ def main(date=None):
     dv_save = open(f'./models/dv-{date}.bin', "wb")
     pickle.dump(dv, dv_save)
 
-
+main('2021-08-15')
+'''
 DeploymentSpec(
     flow=main,
     parameters={'date':'2021-08-15'},
@@ -128,3 +134,4 @@ DeploymentSpec(
     flow_runner=SubprocessFlowRunner(),
     tags=["testing"]
 )
+'''
