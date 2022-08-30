106dd86330c51da2e0ad11f1ed5149fb5b227997
IzicTemi <iomolayo2541@gmail.com>
2022-08-30 08:34:33 +0000

    main

--- 06-best-practices/test_docker.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-with open('event.json', 'rt', encoding='utf-8') as f_in:
-    event = f_in.read()
-
-
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-response = requests.post(url, json=event, timeout=300)
-print(response.json())
